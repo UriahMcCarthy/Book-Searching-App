5ac7ee18e950fea83bcec6434e52597f2eb2fd42
Yuri <uriahmccarthy@gmail.com>
2023-05-24 18:22:38 -0700

    added code and structure for schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String!]
+    description: String!
+    image: String!
+    link: Sting!
+    title: String!
+  }
+
+  input  BookInput {
+    bookId: ID!
+    authors: [String!]
+    description: String!
+    image: String!
+    link: Sting!
+    title: String!
+  }
+ type Auth{
+    token:ID!
+    user:User
+ }
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login (email: String!, password: String!): Auth
+    createUser(username:String!,email:String!,password:String!)
+    saveBook(bookInput: BookInput!): User
+   deleteBook(bookId: ID!):User
+
+  }
+`;
+
+module.exports = typeDefs;
