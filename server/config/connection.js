92b2640c0654eea5cc1733ce18132f4e3e1951ce
Yuri <uriahmccarthy@gmail.com>
2023-06-03 15:59:52 -0700

    fixed db connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+console.log("MONGO URI",process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect("mongodb+srv://uriahmccarthy:uriahmccarthy@cluster0.dacwagj.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = mongoose.connection;
