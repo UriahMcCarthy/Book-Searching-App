fecff59fcde6ebc3bee248352f6047444f503c61
Yuri <uriahmccarthy@gmail.com>
2023-06-05 18:50:55 -0700

    deployment for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-console.log("MONGO URI",process.env.MONGODB_URI)
+//console.log("MONGO URI",process.env.MONGODB_URI)
 
-mongoose.connect("mongodb+srv://uriahmccarthy:uriahmccarthy@cluster0.dacwagj.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Cluster0');
+
+// mongoose.connect("mongodb+srv://uriahmccarthy:uriahmccarthy@cluster0.dacwagj.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = mongoose.connection;
