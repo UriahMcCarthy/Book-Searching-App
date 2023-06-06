367b95fbacc93ddf4a4999ca57bc15be452bb84a
Yuri <uriahmccarthy@gmail.com>
2023-06-05 19:01:50 -0700

    deployment 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //console.log("MONGO URI",process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Cluster0');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0');
 
 // mongoose.connect("mongodb+srv://uriahmccarthy:uriahmccarthy@cluster0.dacwagj.mongodb.net/?retryWrites=true&w=majority");
 
